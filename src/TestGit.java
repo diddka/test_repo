43e9f9a73ccb0ab0e5e303a667dbdc398082af1c
diddka <danailova_rz@abv.bg>
2023-05-09 23:11:09 +0300

    bix the bug

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,6 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test2");
-        System.out.println("upload bug");
+        System.out.println("bug fixed");
     }
 }
